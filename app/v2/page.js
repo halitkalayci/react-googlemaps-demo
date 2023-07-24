1072cca8b7beda9aaf7981abc3d4659d66780d40
halitkalayci <halit.kalayci@ext.etiya.com>
2023-07-24 11:20:43 +0300

    latest

--- app/v2/page.js
+++ app/v2/page.js
@@ -2,18 +2,40 @@
 import { GoogleMapsProvider } from '@ubilabs/google-maps-react-hooks'
 import React, { useCallback, useRef, useState } from 'react'
 
+
+const locations = [
+    { lat: 39, lng: 32, info: 'Deneme' },
+    { lat: 39.213, lng: 32.1235, info: 'Deneme 123' },
+    { lat: 39.69123, lng: 32.123821, info: 'Deneme 321' },
+    { lat: 38.123, lng: 33.2123, info: 'Deneme 123 ' },
+    { lat: 37.231, lng: 33.2131231, info: 'Deneme 321' },
+    { lat: 5, lng: 25, info: 'Deneme 321' },
+]
+
 function Maps() {
     const mapOptions = {
         center: { lat: 39.94826471353634, lng: 32.829480863906234 },
-        zoom: 17
+        zoom: 10
     }
-    const onLoad = (e) => {
-        console.log(e);
+    const onLoad = (map) => {
+        addMarkers(map);
     }
+
+    const addMarkers = (map) => {
+        locations.map(location => {
+            const marker = new google.maps.Marker({
+                map: map,
+                position: { lat: location.lat, lng: location.lng }
+            });
+            //marker.setMap(map);
+        })
+    }
+
     const [mapContainer, setMapContainer] = useState(null)
     const mapRef = useCallback(node => {
         node && setMapContainer(node);
     }, []);
+
     return (
         <GoogleMapsProvider
             googleMapsAPIKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
@@ -21,7 +43,8 @@ function Maps() {
             mapContainer={mapContainer}
             onLoadMap={onLoad}
         >
-            <div ref={mapRef} style={{ height: '100vh' }} />
+            <div ref={mapRef} style={{ height: '100vh' }} >
+            </div>
         </GoogleMapsProvider>
     )
 }

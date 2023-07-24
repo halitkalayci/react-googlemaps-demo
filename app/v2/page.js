9e23dc8409f491e3644859e1dad7f7fa9543e596
halitkalayci <halit.kalayci@ext.etiya.com>
2023-07-24 13:54:31 +0300

    marker clusterer

--- app/v2/page.js
+++ app/v2/page.js
@@ -1,10 +1,17 @@
 "use client"
+import { MarkerClusterer, SuperClusterAlgorithm } from '@googlemaps/markerclusterer'
 import { GoogleMapsProvider } from '@ubilabs/google-maps-react-hooks'
-import React, { useCallback, useRef, useState } from 'react'
+// import { usePlacesAutocomplete } from "@react-google-maps/api"
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 
 
 const locations = [
     { lat: 39, lng: 32, info: 'Deneme' },
+    { lat: 39.210, lng: 32.1235, info: 'Deneme' },
+    { lat: 39.212, lng: 32.1235, info: 'Deneme' },
+    { lat: 39.207, lng: 32.1235, info: 'Deneme' },
+    { lat: 39.205, lng: 32.1235, info: 'Deneme' },
+    { lat: 39.210, lng: 32.1235, info: 'Deneme' },
     { lat: 39.213, lng: 32.1235, info: 'Deneme 123' },
     { lat: 39.69123, lng: 32.123821, info: 'Deneme 321' },
     { lat: 38.123, lng: 33.2123, info: 'Deneme 123 ' },
@@ -13,6 +20,9 @@ const locations = [
 ]
 
 function Maps() {
+
+
+
     const mapOptions = {
         center: { lat: 39.94826471353634, lng: 32.829480863906234 },
         zoom: 10
@@ -22,6 +32,7 @@ function Maps() {
     }
 
     const addMarkers = (map) => {
+        let markers = []
         locations.map(location => {
             const marker = new google.maps.Marker({
                 // map: map,
@@ -34,12 +45,21 @@ function Maps() {
                 infoWindow.open(map);
             })
             marker.setMap(map);
+            markers.push(marker);
         })
+        // npm i @googlemaps/markerclusterer
+        new MarkerClusterer({ markers, map, algorithm: new SuperClusterAlgorithm({ radius: 200 }) })
     }
 
     const [mapContainer, setMapContainer] = useState(null)
     const mapRef = useCallback(node => {
         node && setMapContainer(node);
+    }, []);
+
+    // const placeApi = usePlacesAutocomplete();
+
+    useEffect(() => {
+
     }, []);
 
     return (

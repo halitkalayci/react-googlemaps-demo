948b58b3a9d2780433ed605b83b2bb3b76a6e530
halitkalayci <halit.kalayci@ext.etiya.com>
2023-07-24 14:47:07 +0300

    mock db

--- app/v3/page.js
+++ app/v3/page.js
@@ -1,21 +1,65 @@
 "use client"
 import { VectorMap } from '@react-jvectormap/core'
-import React from 'react'
+import React, { useState } from 'react'
 import { trMill } from "@react-jvectormap/turkey"
 import { plates } from './plates'
 import { map } from './customMap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+const veriler = [
+    {
+        plaka: 34,
+        komurTon: 45,
+        aracSayisi: 12
+    },
+    {
+        plaka: 41,
+        komurTon: 120,
+        aracSayisi: 15
+    },
+    {
+        plaka: 6,
+        komurTon: 512,
+        aracSayisi: 12
+    }
+]
+
+
 function Vectoral() {
+    const [veri, setVeri] = useState(null)
     const onRegionClick = (e, code) => {
-        console.log(code);
         let plate = plates[code];
-        let url = "tki.com/sorgula?plaka=" + plate;
+        let dbdenGelenVeri = veriler.find(i => i.plaka == plate);
+        if (!dbdenGelenVeri) {
+            alert("Bu şehirde veri bulunmuyor..")
+            setVeri(null)
+            return;
+        }
+        setVeri(dbdenGelenVeri);
+        console.log(dbdenGelenVeri);
     }
 
     return (
-        <VectorMap onRegionClick={onRegionClick}
-            className='vmap'
-            map={trMill}>
-        </VectorMap>
+        <>
+            <VectorMap onRegionClick={onRegionClick}
+                className='vmap'
+                map={trMill}>
+            </VectorMap>
+            {veri && <table className='table table-striped'>
+                <thead>
+                    <td>Plaka</td>
+                    <td>Komur Ton</td>
+                    <td>Araç Sayısı</td>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{veri.plaka}</td>
+                        <td>{veri.komurTon}</td>
+                        <td>{veri.aracSayisi}</td>
+                    </tr>
+                </tbody>
+            </table>}
+        </>
+
     )
 }
 

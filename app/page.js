bc1e9b6435e098bc74223c9249f7642aa70fd0d5
halitkalayci <halit.kalayci@ext.etiya.com>
2023-07-24 10:49:06 +0300

    init

--- app/page.js
+++ app/page.js
@@ -1,95 +1,32 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
-
+import { useLoadScript, GoogleMap, Marker, InfoWindow } from "@react-google-maps/api"
+import { useRef } from 'react'
 export default function Home() {
+  const map = useRef(null);
+
+  const script = useLoadScript({ googleMapsApiKey: "" });
+  // 39.94826471353634, 32.829480863906234
+  const openInfoWindow = () => {
+    const infoWindow = new google.maps.InfoWindow({});
+
+    infoWindow.setPosition({ lat: 39.94826471353634, lng: 32.829480863906234 });
+    infoWindow.setContent(`<h3>Türkiye Kömür İşletmeleri </h3>`)
+    infoWindow.open(map);
+  }
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      {!script.isLoaded && <div>Harita yükleniyor...</div>}
+      {script.isLoaded &&
+        <div className='map-wrapper'>
+          <GoogleMap ref={map} onLoad={(e) => console.log("Map yüklendi..")} onClick={(e) => console.log(e)} center={{ lat: 39.94826471353634, lng: 32.829480863906234 }} mapContainerClassName='map' zoom={17}>
+            <Marker onClick={(e) => { openInfoWindow(e) }} position={{ lat: 39.94826471353634, lng: 32.829480863906234 }}>
+            </Marker>
+          </GoogleMap>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      }
+    </>
   )
 }
+// npm i @react-google-maps/api

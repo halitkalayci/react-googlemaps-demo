b115d9d401c4c49d00af14c5f678f3b718ad2feb
halitkalayci <halit.kalayci@ext.etiya.com>
2023-07-24 11:05:22 +0300

    useCallback

--- app/v2/page.js
+++ app/v2/page.js
@@ -1,6 +1,6 @@
 "use client"
 import { GoogleMapsProvider } from '@ubilabs/google-maps-react-hooks'
-import React, { useRef } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 
 function Maps() {
     const mapOptions = {
@@ -10,14 +10,18 @@ function Maps() {
     const onLoad = (e) => {
         console.log(e);
     }
-    const mapRef = useRef();
+    const [mapContainer, setMapContainer] = useState(null)
+    const mapRef = useCallback(node => {
+        node && setMapContainer(node);
+    }, []);
     return (
         <GoogleMapsProvider
             googleMapsAPIKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
             mapOptions={mapOptions}
+            mapContainer={mapContainer}
             onLoadMap={onLoad}
         >
-            <div ref={mapRef} style={{ height: '100%' }} />
+            <div ref={mapRef} style={{ height: '100vh' }} />
         </GoogleMapsProvider>
     )
 }
